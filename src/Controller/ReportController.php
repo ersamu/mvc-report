49e808a39aa37292201b15d19c6c6cb0b5576401
Eric Samuelsson <gossen02@gmail.com>
2022-04-05 12:11:52 +0100

    Finished exercises for kmom01

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/report", name="report")
+     */
+    public function report(): Response
+    {
+
+        return $this->render('report.html.twig');
+    }
+}
