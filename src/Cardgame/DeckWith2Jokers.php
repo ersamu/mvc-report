de959933ef334d80d079f7f3febbeab0527a9d73
Eric Samuelsson <gossen02@gmail.com>
2022-04-22 21:08:52 +0100

    Added types

--- src/Cardgame/DeckWith2Jokers.php
+++ src/Cardgame/DeckWith2Jokers.php
@@ -2,12 +2,14 @@
 
 namespace App\Card;
 
+use App\Controller;
+
 class DeckWith2Jokers extends Deck
 {
-    public function addJokers()
+    public function addJokers(): void
     {
         for ($x = 0; $x < 2; $x++) {
-            array_push($this->allCards, new \App\Card\Card("\u{1F0BF}", "JO"));
+            array_push($this->allCards, new Card("\u{1F0BF}", "JO"));
         }
     }
 }
