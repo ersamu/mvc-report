c06a8f2a735d1ff20d5aee1ce6c65058eb168152
Eric Samuelsson <gossen02@gmail.com>
2022-04-22 21:08:34 +0100

    Added types

--- src/Cardgame/Deck.php
+++ src/Cardgame/Deck.php
@@ -2,12 +2,14 @@
 
 namespace App\Card;
 
+use App\Controller;
+
 class Deck
 {
-    protected $allCards;
-    private $allSuites = ["\u{2665}", "\u{2666}", "\u{2660}", "\u{2663}"];
-    private $allValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-    private $remainingCards;
+    protected array $allCards;
+    private array $allSuites = ["\u{2665}", "\u{2666}", "\u{2660}", "\u{2663}"];
+    private array $allValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    private array $remainingCards;
 
     public function __construct()
     {
@@ -15,34 +17,34 @@ class Deck
         $this->addCards();
     }
 
-    private function addCards()
+    private function addCards(): void
     {
         foreach ($this->allSuites as $suit) {
             foreach ($this->allValues as $value) {
-                array_push($this->allCards, new \App\Card\Card($suit, $value));
+                array_push($this->allCards, new Card($suit, $value));
             }
         }
         $this->remainingCards = $this->allCards;
     }
 
-    public function getAllCards()
+    public function getAllCards(): array
     {
         return $this->allCards;
     }
 
-    public function getCards()
+    public function getCards(): array
     {
         return $this->remainingCards;
     }
 
-    public function shuffleCards()
+    public function shuffleCards(): array
     {
         $this->remainingCards = $this->allCards;
         shuffle($this->remainingCards);
-        return $this->remainingCards;
+        return $this->getCards();
     }
 
-    public function drawCard(int $nrOfCards)
+    public function drawCard(int $nrOfCards): array
     {
         $usedCards = [];
         for ($x = 0; $x < $nrOfCards; $x++) {
@@ -51,7 +53,7 @@ class Deck
         return $usedCards;
     }
 
-    public function cardsLeft()
+    public function cardsLeft(): int
     {
         return count($this->remainingCards);
     }
