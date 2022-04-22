ede7a7fc2a001959b2a8e6b84b524592ae4f19db
Eric Samuelsson <gossen02@gmail.com>
2022-04-22 21:15:06 +0100

    Add new validation tool

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '582c933edbc7d215795aa1394202f68f81d98718',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '582c933edbc7d215795aa1394202f68f81d98718',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.5.7',
+            'version' => '1.5.7.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '7fb7e2e1e9f3d59a26a413b2d3d5e47f0edb75ac',
+            'dev_requirement' => false,
+        ),
+    ),
+);
